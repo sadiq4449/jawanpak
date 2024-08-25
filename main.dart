418bb4afbef7a5def04cb6b97948aa1094daa3cb
sadiq4449 <sadiqalibhutto1214@gmail.com>
2024-08-25 04:49:50 -0700

    jawan pak practics

--- main.dart
+++ main.dart
@@ -8,10 +8,16 @@ void main() {
   var a = true;
   var b = true;
   var c = a && b;
+  //c is true
   var d = !a || (b && !c);
+  // d is flase
   var e = !c || (!d && !c || b);
+  // e is true
   var f = !e || (b && !e || !a);
+  // f is false
   var g = (f || b) && !e || !d;
+  print("sadiq shigri ");
+
   print(d);
   print(e);
   print(f);
